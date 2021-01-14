723c345b43386839942d1f229b3e597334f849ed
Tina <1098299287@qq.com>
2021-01-14 20:25:25 +0800

    Topnav组件封装好了

---
+++ src/views/Home.vue
@@ -0,0 +1,99 @@
+<template>
+    <div>
+      <Topnav />
+        <div class="banner">
+            <h1>轱辘UI</h1>
+            <h2>一个牛逼的UI框架</h2>
+            <p class="actions"> 
+                <a>Github</a>
+                <a>开始</a>
+            </p>
+        </div>
+    </div>  
+</template>
+
+<script lang="ts">
+import Topnav from '../components/Topnav.vue'
+export default {
+  components:{Topnav:Topnav}
+}
+</script>
+
+<style lang="scss" scoped>
+$green: #02bcb0;
+$border-radius: 4px;
+$color: #007974;
+.topnavAndBanner {
+  background: linear-gradient(145deg, rgba(227, 255, 253, 1) 0%, rgba(183, 233, 230, 1) 100%);
+  clip-path: ellipse(80% 60% at 50% 40%);
+}
+.features {
+  margin: 64px auto;
+  padding: 0 16px;
+  @media (min-width: 800px) {
+    width: 800px;
+    >ul {
+      >li {
+        width: 50%;
+      }
+    }
+  }
+  @media (min-width: 1200px) {
+    width: 1200px;
+    >ul {
+      >li {
+        width: 33.3333%;
+      }
+    }
+  }
+  >ul {
+    display: flex;
+    flex-wrap: wrap;
+    >li {
+      margin: 16px 0;
+      display: grid;
+      justify-content: start;
+      align-content: space-between;
+      grid-template-areas:
+        "icon title"
+        "icon text";
+      grid-template-columns: 80px auto;
+      grid-template-rows: 1fr auto;
+      >svg {
+        grid-area: icon;
+        width: 64px;
+        height: 64px;
+      }
+      >h3 {
+        grid-area: title;
+        font-size: 28px;
+      }
+      >p {
+        grid-area: text
+      }
+    }
+  }
+}
+.banner {
+  color: $color;
+  padding: 100px 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  >.actions {
+    padding: 8px 0;
+    a {
+      margin: 0 8px;
+      background: $green;
+      color: white;
+      display: inline-block;
+      padding: 8px 24px;
+      border-radius: $border-radius;
+      &:hover {
+        text-decoration: none;
+      }
+    }
+  }
+}
+</style>
